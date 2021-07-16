399d110873db26dbda8a44fe23494f6b15950454
jackson22burns <jackson22.burns@gmail.com>
2021-07-16 17:54:33 -0400

    Uploaded documents create new row in postgres related to the users id

--- server/database.sql
+++ server/database.sql
@@ -14,10 +14,21 @@ ADD COLUMN user_bus VARCHAR(255),
 ADD COLUMN ticket_num VARCHAR(255);
 
 
-INSERT INTO users (user_address, user_bus, ticket_num)
- VALUES ('5716 Gilesburg Ct. McAllen, TX', 'JD Catering For You', '100100100')
- WHERE user_name = 'John Doe';
-
 UPDATE users SET user_address = '5716 Gilesburg Ct. McAllen, TX' WHERE user_name = 'John Doe';
 UPDATE users SET user_bus = 'JD Catering For You' WHERE user_name = 'John Doe';
 UPDATE users SET ticket_num = '100100100' WHERE user_name = 'John Doe';
+
+
+CREATE TABLE documents (
+    doc_id uuid DEFAULT uuid_generate_v4(),
+    doc_path VARCHAR(255),
+    user_id uuid,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+/*changed to this because uuid_generator wasnt working...*/
+CREATE TABLE documents (
+    doc_id BigInt,
+    doc_path VARCHAR(255),
+    user_id uuid,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -21,7 +21,7 @@ router.get("/", authorize, async (req, res) => {
   try {
     //res.json(req.user);
     const user = await pool.query(
-      "SELECT user_name, user_address, user_bus, ticket_num FROM users WHERE user_id = $1",
+      "SELECT user_id, user_name, user_address, user_bus, ticket_num FROM users WHERE user_id = $1",
       [req.user.id] 
     ); 
     res.json(user.rows[0]);
@@ -32,7 +32,15 @@ router.get("/", authorize, async (req, res) => {
 });
 
 router.post('/documents', upload.single("upload"), (req, res) => {
-  console.log(req.file);
+  const { user_id } = req.headers;
+  const u_id = user_id.replace(/['"]+/g, ''); //need to get rid of double quotes in order to add to DB
+  const path = req.file.path;
+  
+  //insert document path into Postgres
+  pool.query(
+    "INSERT INTO documents (user_id, doc_path) VALUES ($1, $2) RETURNING *",
+    [u_id, path]
+  );
   res.send("Single File upload success");
 });
 
